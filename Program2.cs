3b24da476279e4e796131a8349f91f8bf9969c65
Project1 <project1@example.com>
2026-05-09 17:20:13 +0900

    Add example utilities and Program2

---
+++ Program2.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program2
+{
+    static void Main()
+    {
+        Hello.SayHello();
+        Console.WriteLine($"5 is even: {MathUtil.IsEven(5)}");
+    }
+}
---
+++ examples/MathUtil.cs
@@ -0,0 +1,4 @@
+public static class MathUtil
+{
+    public static bool IsEven(int x) => x % 2 == 0;
+}
